baade2c1660a2fe299915073e81051f4d77bba92
郝文韬 <haowentao@tian028.com>
2025-09-03 15:47:28 +0800

    feat: 添加应用启动日志- 在主函数中添加了日

--- src/main/kotlin/com/taoyuan/stardewvalleysavessync/StardewValleySavesSyncApplication.kt
+++ src/main/kotlin/com/taoyuan/stardewvalleysavessync/StardewValleySavesSyncApplication.kt
@@ -1,11 +1,15 @@
 package com.taoyuan.stardewvalleysavessync
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class StardewValleySavesSyncApplication
 
+
 fun main(args: Array<String>) {
     runApplication<StardewValleySavesSyncApplication>(*args)
+    val logger = LoggerFactory.getLogger(StardewValleySavesSyncApplication::class.java)
+    logger.info("应用启动成功！")
 }
